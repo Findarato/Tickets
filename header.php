a06562f910d92da986a749ee53508e97e374d68f
Joseph Harry <findarato@gmail.com>
2010-02-17 22:07:57 +0000

    Fixed the close and reopen ticket errors.

--- header.php
+++ header.php
@@ -1,14 +1,6 @@
 <?php
 include_once "small_header.php";
-function getUrl() {	$turi = strtolower(str_replace("%20"," ",$_SERVER["REQUEST_URI"]));	$data = split("/",$turi);	$data = array_slice($data,1,count($data)-1);return $data;}
-
-//Smarty header
-/**
- * Setup the connection to smarty and set some variables
- */
-
+//function getUrl() {	$turi = strtolower(str_replace("%20"," ",$_SERVER["REQUEST_URI"]));	$data = split("/",$turi);	$data = array_slice($data,1,count($data)-1);return $data;}
 include_once "smarty.inc.php";
-
-//this is a test
 ?>  
      
