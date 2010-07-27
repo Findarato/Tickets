d04f11eb871c858890ae30f4389c6942f9fee5d2
Joseph Harry <findarato@gmail.com>
2010-07-27 18:56:31 +0000

    User pages now show the correct data for the Tickets created. Fixed a small issue that was causing all tickets to be created by nagios.

--- ajax/get_userinfo.php
+++ ajax/get_userinfo.php
@@ -17,10 +17,43 @@ FROM
 WHERE 
 	id=".$_GET["userId"]
 ,false,"assoc");
+$months = array(1,2,3,4,5,6,7,8,9,10,11,12);
+$monthLables = array();
+for($a=1;$a<13;$a++){
+	$monthLables[] = date("M",mktime(0,0,0,$a,1,1982));
+}
+$endMonths = array_slice($months,0,date("m"));
+$startMonths = array_slice($months,date("m"));
+$newMonths = array();
+$nmh = array();
+$displayData = array();
 
+array_push($newMonths,$startMonths);
+array_push($newMonths,$endMonths);
+$newMonths = array_implode($newMonths);
+$newMonthsHold = array(1,2,3,4,5,6,7,8,9,10,11,12);
+$yearStart = date("Y-m-d",mktime(0,0,0,date("m")+1,1,date("Y")-1));
+$yearEnd = date("Y-m-d",mktime(0,0,0,date("m"),31,date("Y")));
 //Tickets
 $json["tickets"]["byMe"] = $db->Query("SELECT count(id) FROM tickets WHERE tickettype_id=1 AND created_by_id=".$_GET["userId"],false,"row");
 $json["tickets"]["toMe"] = $db->Query("SELECT count(id) FROM tickets WHERE tickettype_id=1 AND assigned_id=".$_GET["userId"]." AND open=1",false,"row");
+$graphData = $db->Query("SELECT MONTH(created_on) AS month,count(*) AS total,YEAR(created_on) AS year FROM tickets WHERE created_on BETWEEN '".$yearStart."' AND '".$yearEnd."' AND tickettype_id=1 AND created_by_id=".$_GET["userId"]." GROUP BY MONTH(created_on),YEAR(created_on) ORDER BY YEAR(created_on),MONTH(created_on)",false,"assoc");
+$cnt = 0;
+foreach($newMonths as $nm){
+	$cnt++;
+	foreach($graphData as $key=>$gd){
+		if($gd["month"] == $nm){
+			$displayData[$cnt] = $graphData[$key];
+			break;
+		}else{
+			$displayData[$cnt] = array("month"=>$nm,"total"=>0);
+		}
+	}
+}
+$json["tickets"]["graph"]["byMe"]["data"] = $displayData;
+$json["tickets"]["graph"]["monthLables"] = $monthLables;
+//$json["tickets"]["graph"]["byMe"]["lables"] = $labelData;
+
 
 //Bugs
 $json["bugs"]["byMeOpen"] = $db->Query("SELECT count(id) FROM tickets WHERE created_by_id=".$_GET["userId"]." AND open=1 AND tickettype_id=2",false,"row");
@@ -29,6 +62,6 @@ $json["bugs"]["byMe"] = $db->Query("SELECT count(id) FROM tickets WHERE created_
 //Responses
 $json["responses"]["created"] = $db->Query("SELECT count(id) FROM responses WHERE user_id=".$_GET["userId"],false,"row");
 
+//echo indentJson(json_encode($json));
 echo json_encode($json);
-?> 
-   
+?>    
