80028cb34d6570c24245b8b6a1675775a950c82d
Joseph Harry <findarato@gmail.com>
2010-10-21 20:14:45 +0000

    Bug display is now working properly.

--- ajax/edit_ticket.php
+++ ajax/edit_ticket.php
@@ -5,17 +5,11 @@
  * @version 1.0
  * @started September 14, 2010
  */
-include_once("../small_header.php");
+include("../small_header.php");
+include("../smarty.inc.php");
 header('Content-type: application/json');
 $usr = unserialize($_SESSION['user']);
 $_GET = $db->Clean($_GET,true);
-
-
-if(isset($_GET["debug"])){
-  echo "<pre>";
-  print_r($_GET);
-  echo "</pre>";
-}
 
 $sql = "UPDATE tickets.tickets SET ";
 $setArray = array();
@@ -43,12 +37,21 @@ if(isset($_GET["edit"]) && $_GET["edit"]==1){
 }
 $sql .= join(",",$setArray);
 $sql .= " WHERE id=".$_GET["ticketId"]. " LIMIT 1;";
+if(isset($_GET["debug"])){
+  echo "<pre>";
+  print_r($_GET);
+  echo "</pre>";
+  echo $sql;
+  die();
+}
 $db->Query($sql);
 if(count($db->Error)==2){
   $response["error"] = $db->Error;
 }else{
   $response["message"] = "Ticket Updated Successfully";
   $response["modifiedTicket"] = $_GET["ticketId"];
+  $response["modifiedTicketBody"] = nl2br(Tcode($_GET["ticketBody"]));
+  addReply($_GET["ticketId"], $usr->User_id, "Ticket Modified", "This ticket was modified on ".date("D M j, Y G:i:s "). " by [user=".$usr->User_id."]");
 }
 echo json_encode($response);
 ?>
