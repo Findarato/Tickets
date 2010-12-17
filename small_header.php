acdb4c42b1c52e376f83ffdd71a99475b4991ded
Joseph Harry <findarato@gmail.com>
2010-12-17 18:10:35 +0000

    Live Ticket Update XXI v.3.1.0.r314 Bug Fixes Fixed bug #545. Fixed bug #693.
    New Features
    Creating a new ticket, or a new bug report now shows up inside of the ticket display instead of as a popup.
    Adding a reply now follows the same look and feel of new tickets and bug reports.
    
    Known Issues
    Searching By project does not work
    There are no page numbers, nor any way to show more then the default amount of tickets in the list view.
    There are some issues with editing that have been around since editing was in place. All of the ticket code links are not working as intended with the new editing system.
    Closed tickets are not showing up for some people. Even though a number shows up next to Closed Tickets.

---
+++ small_header.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Small header that can be included if no smarty access is needed
+ * @author Joseph Harry
+ * @version 1.0
+ * @copyright March 28, 2009
+ */
+
+ 
+
+$lapcatPath = "/www/live";
+$ticketsPath="/www/tickets";
+
+if($_SERVER['DOCUMENT_ROOT'] != $ticketsPath){
+	$_SERVER['DOCUMENT_ROOT'] = $ticketsPath;
+}
+
+
+session_start();//start the session
+$response = array("message"=>"","error"=>""); //including the generatic response
+function __autoload($class_name) { require_once $_SERVER['DOCUMENT_ROOT']."/classes/".strtolower($class_name) . '.class.php'; }
+require_once "functions.php";
+require_once $lapcatPath."/lapcat/objects/user.php";
+require_once $lapcatPath."/lapcat/code/php-functions.php";
+//require_once "/www/live/lapcat/code/achievements.php";
+
+$db = db::getInstance();
+//$DateTimeZone = timezone_open ( 'America/Chicago' );
+date_default_timezone_set( 'America/Chicago' );
+ 
+?>  
