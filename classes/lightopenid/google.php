1419a2f19bb7c6aefac96223089ae6001f3ce92a
Joseph Harry <findarato@gmail.com>
2011-07-12 15:49:56 +0000

    Adding login javascript file.  Pulling out things that need to happen for just login.  Also adding in all of the css needed to make the login page fit with the rest of tickets.

--- classes/lightopenid/google.php
+++ classes/lightopenid/google.php
@@ -3,6 +3,26 @@ require 'openid.php';
 require "../../small_header.php";
 $_SESSION["validOpenID"] = false;
 $_SESSION["openID"] = array();
+?>
+<html>
+	<head>
+	 	<link id="themegencss" href="/tickets/css/themes/default/style.css" rel="stylesheet" />
+        <link rel="stylesheet" media="screen" href="css/buttons.css?v=21"  /> 
+        <link rel="stylesheet" media="screen" href="css/tickets.css?v=22"  /> 
+        <link rel="shortcut icon" href="/tickets/bug.png">
+        <script type="text/javascript">
+            var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+            document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+        </script>
+        <script type="text/javascript">
+            var pageTracker = _gat._getTracker("UA-8067208-4");
+            pageTracker._trackPageview();
+        </script>
+		<script src="http://ajax.microsoft.com/ajax/jQuery/jquery-1.5.2.min.js"></script>
+		<script src="js/login.js"></script>
+	</head>
+	<body>
+		<?php
 try {
     $openid = new LightOpenID;
     if(!$openid->mode) {
@@ -18,9 +38,9 @@ try {
         if($openid->validate())
         {
         	//require "/classes/"
-            echo 'User <b>' . $openid->identity . '</b> has logged in.<br>';
+           // echo 'User <b>' . $openid->identity . '</b> has logged in.<br>';
  
-            echo "<h3>User information</h3>";
+            //echo "<h3>User information</h3>";
  
             $identity = $openid->identity;
             $attributes = $openid->getAttributes();
@@ -43,20 +63,22 @@ try {
 			$_SESSION["openID"]["last_name"] = $last_name;
 		
 			$user_id = $db->Query("SELECT id FROM tickets.users WHERE email_address ='".$email."' ",false,"row");
-
-			if($res == 0){
+			
+			if($user_id == 0){ //We did not find a account to link
 				
-			}else{
-				print_r($res);
+			}else{ //There is an account with the same email address
+				//print_r($user_id);
 				$_SESSION["validOpenID"] = true;
 				$openIdtoUserID = $db->Query("SELECT user_id,open_id FROM tickets.openId_users WHERE user_id ='".$User_id."' ",false,"row");
 				if($openIdtoUserID == 0){ //There is no link in the openID table
-					echo "There is no Google ID link<br>";
-					echo "If you have a Tickets account please login with that now.";
-					echo "<button>Close</button>";		
+					echo "There is an account with the same email address found. Do you want to link it?";
+					echo "<button id='yesLink'>Yes</button>";
+					echo "<button id='noLink'>No</button>";		
 				}else{//There is a link to a user id in the table.
-					
-							
+					//echo "There is no Google ID link<br>";
+					//echo "If you have a Tickets account please login with that now.";
+					//echo "<button>Close</button>";	
+
 				}
 			}
         }
@@ -69,3 +91,5 @@ try {
     echo $e->getMessage();
 }
 ?>
+	</body>	
+</html>
---
+++ js/login.js
@@ -0,0 +1,10 @@
+/*
+ * Login script.  Will do the ajax and any other random stuff that needs to happen only for the login screen
+ * 
+ */
+
+
+jQuery(document).ready(function () {
+
+
+});
