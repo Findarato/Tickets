b8475ba671ab0c3c07cb8487e0a4b0f0c94bf1a9
Joseph Harry <findarato@gmail.com>
2011-07-12 15:17:46 +0000

    Getting the whole flow to login working.  openID information will be stored in the session for future use.

--- classes/lightopenid/google.php
+++ classes/lightopenid/google.php
@@ -2,6 +2,7 @@
 require 'openid.php';
 require "../../small_header.php";
 $_SESSION["validOpenID"] = false;
+$_SESSION["openID"] = array();
 try {
     $openid = new LightOpenID;
     if(!$openid->mode) {
@@ -26,12 +27,21 @@ try {
             $email = $attributes['contact/email'];
             $first_name = $attributes['namePerson/first'];
             $last_name = $attributes['namePerson/last'];
- 
+ 			
+ 			/*
             echo "mode: " . $openid->mode . "<br>";
             echo "identity: " . $identity . "<br>";
             echo "email: " . $email . "<br>";
             echo "first_name: " . $first_name . "<br>";
             echo "last_name: " . $last_name . "<br>";
+			*/
+
+			$_SESSION["openID"]["email"] = $email;
+			$_SESSION["openID"]["mode"] = $openid->mode;
+			$_SESSION["openID"]["identity"] = $identity;
+			$_SESSION["openID"]["first_name"] = $first_name;
+			$_SESSION["openID"]["last_name"] = $last_name;
+		
 			$user_id = $db->Query("SELECT id FROM tickets.users WHERE email_address ='".$email."' ",false,"row");
 
 			if($res == 0){
@@ -41,7 +51,9 @@ try {
 				$_SESSION["validOpenID"] = true;
 				$openIdtoUserID = $db->Query("SELECT user_id,open_id FROM tickets.openId_users WHERE user_id ='".$User_id."' ",false,"row");
 				if($openIdtoUserID == 0){ //There is no link in the openID table
-					
+					echo "There is no Google ID link<br>";
+					echo "If you have a Tickets account please login with that now.";
+					echo "<button>Close</button>";		
 				}else{//There is a link to a user id in the table.
 					
 							
