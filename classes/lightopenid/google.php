61c8025e07aa00c9f601d394b99f458dc5beef35
Joseph Harry <findarato@gmail.com>
2011-07-18 19:22:11 +0000

    You can now link any google account to a ticket account even if you already have one linked.

--- classes/lightopenid/google.php
+++ classes/lightopenid/google.php
@@ -6,7 +6,8 @@ require "../../small_header.php";
 $_SESSION["validOpenID"] = false;
 $_SESSION["openID"] = array();
 
-if(isset($_SESSION["usr"])){$usr = unserilize($_SESSION["usr"]);}
+if(isset($_SESSION["user"])){$usr = unserialize($_SESSION["user"]);}
+
 ?>
 <html>
 	<head>
@@ -55,13 +56,20 @@ try {
 		
 		
 			//echo $identity;
-			$user_id = $db->Query("SELECT id,firstname,lastname,username FROM tickets.users WHERE email_address ='".$email."' ",false,"assoc");
+			$userId = $db->Query("SELECT id,firstname,lastname,username FROM tickets.users WHERE email_address ='".$email."' ",false,"assoc");
+			if($userId != 0){
+				$openIdtoUserID = $db->Query("SELECT user_id,open_id FROM tickets.openId_users WHERE user_id ='".$userId["id"]."'AND open_id='".$identity."';",false,"row");	
+			}else{
+				$openIdtoUserID = $db->Query("SELECT user_id,open_id FROM tickets.openId_users WHERE open_id='".$identity."';",false,"row");
+				$userId["id"] = $openIdtoUserID[0];
+			}
 				
-			if($user_id == 0){ //We did not find a account to link
-				if($usr){
+			if($userId == 0 && $openIdtoUserID == 0){ //We did not find a account to link
+				if(isset($usr) && isset($usr->User_id)){
 					$db->Query("INSERT INTO openId_users (user_id,open_id) VALUES (".$usr->User_id.",'".mysql_real_escape_string($_SESSION["openID"]["identity"])."')");
 					if(count($db->Error==2)){//There was an error
 						echo "There was an error trying to link your accounts.";
+						print_r($db->Error);
 					}else{
 						echo "Your account is now linked with your google account";
 					}
@@ -70,11 +78,10 @@ try {
 					echo "Please log in to tickets to link your email address.<br>";
 					echo "<button class='minimal ticketPadding3' id='noLink' style='width:100px;'>Close</button>";
 				}
-				//print_r($user_id);
+				//print_r($userId);
 			}else{ //There is an account with the same email address
 				$_SESSION["validOpenID"] = true;
-				//$_SESSION["openID"]["user_id"] = $user_id;
-				$openIdtoUserID = $db->Query("SELECT user_id,open_id FROM tickets.openId_users WHERE user_id ='".$user_id["id"]."' ",false,"row");
+				//$_SESSION["openID"]["user_id"] = $userId;
 				if($openIdtoUserID == 0){ //There is no link in the openID table
 					echo "There is an account with the same email address found. Do you want to link it to this Google ID?<br>";
 					echo "<button class='minimal ticketPadding3' id='yesLink' style='width:100px;'>Yes</button>";
@@ -82,13 +89,11 @@ try {
 				}else{//There is a link to a user id in the table.
 					echo "Thank you for logging into ticket with a google account.  We will now fully log you into tickets<br>"; 
 					//require ($_SERVER["DOCUMENT_ROOT"]."/ajax/login.php");
-					$password = $db->Query("SELECT password FROM users WHERE id=".$user_id["id"],false,"row");
-					$response = login($user_id["id"],$password,$response,true);
+					$password = $db->Query("SELECT password FROM users WHERE id=".$userId["id"],false,"row");
+					$response = login($userId["id"],$password,$response,true);
 					
 					//print_r($response);die("died");
-					echo '<script type="text/javascript">
-						window.opener.window.login('.json_encode($response).');
-					</script>';
+					echo '<script type="text/javascript">window.opener.window.login('.json_encode($response).');window.close();</script>';
 					
 				}
 			}
