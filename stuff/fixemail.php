a8728fed8a6b2232b7b3d60e21c81827de5a7902
Joseph Harry <findarato@gmail.com>
2011-07-26 20:21:05 +0000

    added some caution

--- stuff/fixemail.php
+++ stuff/fixemail.php
@@ -5,7 +5,7 @@
 	$users = $db->Query("SELECT user_id,email FROM tickets.alt_email",false,"assoc_array");
 	
 	foreach($users as $u){
-		echo "UPDATE users SET email_address='".$u["email"]."' WHERE id=".$u["user_id"].";";
+		echo "UPDATE users SET email_address='".$u["email"]."' WHERE id=".$u["user_id"]." LIMIT 1;";
 	} 
 
 ?>
