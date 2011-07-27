656012880764b58d95db26aa0c4f4eb41126b9ff
Joseph Harry <findarato@gmail.com>
2011-07-27 16:28:22 +0000

    Release candidate for version 3.5

---
+++ ajax/get_projects.php
@@ -0,0 +1,16 @@
+<?Php
+/**
+ * This script will get all of the categories and return them in a json string.
+ * @author Joseph Harry
+ * @version 1.0
+ * @started September 14, 2010
+ */
+include_once("../small_header.php");
+header('Content-type: application/json');
+$usr = unserialize($_SESSION['user']);
+$_GET = $db->Clean($_GET);
+
+$projects = $db->Query("SELECT id,name FROM tickets.projects;",false,"assoc");
+
+echo json_encode(array("projects"=>$projects));
+?>
