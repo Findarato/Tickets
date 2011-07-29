126d236376ba1e0a3f9a272781186588aef75e55
Joseph Harry <findarato@gmail.com>
2011-07-29 16:21:02 +0000

    Release Candidate 2 fixed a bunch of bugs that got though the first RC

---
+++ header.php
@@ -0,0 +1,6 @@
+<?php
+include_once "small_header.php";
+//function getUrl() {	$turi = strtolower(str_replace("%20"," ",$_SERVER["REQUEST_URI"]));	$data = split("/",$turi);	$data = array_slice($data,1,count($data)-1);return $data;}
+include_once "smarty.inc.php";
+?>  
+     
