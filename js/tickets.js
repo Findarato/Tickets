779718b0bfca12a07ef04e87a42d853242b80fb8
Joseph Harry <findarato@gmail.com>
2011-08-03 18:11:35 +0000

    Fixed a bug stopping ticket specific email from displaying properly.

--- js/tickets.js
+++ js/tickets.js
@@ -1066,7 +1066,7 @@ function loadUserPage(userId){
 		Tlb.find("#realName").append(data.userInfo.firstname+" "+data.userInfo.lastname);
 		Tlb.find("#joinedOn").append(data.userInfo.joined);
 		Tlb.find("#userType").append(data.userInfo.type);
-		Tlb.find("#userSecondaryEmail").val(data.userInfo.tickets.altEmail);
+		Tlb.find("#userSecondaryEmail").val(data.userInfo.tickets.email);
 		Tlb
 			.find("#totalTickets")
 				.append(data.tickets.byMe)
