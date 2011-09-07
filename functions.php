652e19099271a9af4cf031540f790efa58a4ce78
Joseph Harry <findarato@gmail.com>
2011-09-07 17:31:11 +0000

    added the proper classes to the links to make them look like ticket links

--- functions.php
+++ functions.php
@@ -26,10 +26,9 @@ function id2Username($user_id,$userTable="users",$userDatabase="tickets" ){
 function toLink($text){
 	$text = html_entity_decode($text);
 	$text = " ".$text;
-	$text = eregi_replace('(((f|ht){1}tp://)[-a-zA-Z0-9@:%_\+.~#?&//=]+)','<a href="\\1">\\1</a>', $text);
-	$text = eregi_replace('(((f|ht){1}tps://)[-a-zA-Z0-9@:%_\+.~#?&//=]+)','<a href="\\1">\\1</a>', $text);
-	$text = eregi_replace('([[:space:]()[{}])(www.[-a-zA-Z0-9@:%_\+.~#?&//=]+)','\\1<a href="http://\\2">\\2</a>', $text);
-	$text = eregi_replace('([_\.0-9a-z-]+@([0-9a-z][0-9a-z-]+\.)+[a-z]{2,3})','<a href="mailto:\\1">\\1</a>', $text);
+	$text = eregi_replace('(((f|ht){1}tp://)[-a-zA-Z0-9@:%_\+.~#?&//=]+)','<a class="globe fakelink ticket_button ticket_sprite" href="\\1">\\1</a>', $text);
+	$text = eregi_replace('(((f|ht){1}tps://)[-a-zA-Z0-9@:%_\+.~#?&//=]+)','<a class="globe fakelink ticket_button ticket_sprite" href="\\1">\\1</a>', $text);
+	$text = eregi_replace('([[:space:]()[{}])(www.[-a-zA-Z0-9@:%_\+.~#?&//=]+)','\\1<a class="globe fakelink ticket_button ticket_sprite" href="http://\\2">\\2</a>', $text);
 	return $text;
 }
 function aTcode($array,$id='description'){
