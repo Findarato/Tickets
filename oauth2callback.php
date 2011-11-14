4650db1734b8418705e06f5dc32e56cfc39296b8
Joseph Harry <findarato@gmail.com>
2011-11-14 15:14:23 +0000

    Removed the secret information from the callback test page

--- oauth2callback.php
+++ oauth2callback.php
@@ -2,39 +2,86 @@
 session_start();
 
 // In a real application this would use a database, and not a session!
-require_once '/stuff/google-api-php-client/src/apiClient.php';
-require_once '/stuff/google-api-php-client/src/contrib/apiPlusService.php';
+require_once 'stuff/google-api-php-client/src/apiClient.php';
+require_once 'stuff/google-api-php-client/src/contrib/apiPlusService.php';
 session_start();
 
 $client = new apiClient();
 $client->setApplicationName('Google+ PHP Starter Application');
 // Visit https://code.google.com/apis/console?api=plus to generate your
 // client id, client secret, and to register your redirect uri.
-$client->setClientId('750669202824.apps.googleusercontent.com');
-$client->setClientSecret('bxVPjPk2M6Gm8Ys0mRE8QUTC');
-$client->setRedirectUri('http://devtickets.lapcat.org/oauth2callback.php');
-$client->setDeveloperKey('AIzaSyCFh5oGRPL3J15oi08tUMqITBKeBtEFzyw');
+include "googleapi.inc.php";
+$client->setClientId($clinetId);
+$client->setClientSecret($clinetSecret);
+$client->setRedirectUri($clientRedirect);
+$client->setDeveloperKey($clientDeveloperKey);
+$client->setScopes(array('https://www.googleapis.com/auth/plus.me'));
 $plus = new apiPlusService($client);
+
+if (isset($_REQUEST['logout'])) {
+  unset($_SESSION['access_token']);
+}
 
 if (isset($_GET['code'])) {
   $client->authenticate();
-  $_SESSION['token'] = $client->getAccessToken();
+  $_SESSION['access_token'] = $client->getAccessToken();
   header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
 }
 
-if (isset($_SESSION['token'])) {
-  $client->setAccessToken($_SESSION['token']);
+if (isset($_SESSION['access_token'])) {
+  $client->setAccessToken($_SESSION['access_token']);
 }
 
 if ($client->getAccessToken()) {
+  $me = $plus->people->get('me');
+
+  // These fields are currently filtered through the PHP sanitize filters.
+  // See http://www.php.net/manual/en/filter.filters.sanitize.php
+  $url = filter_var($me['url'], FILTER_VALIDATE_URL);
+  $img = filter_var($me['image']['url'], FILTER_VALIDATE_URL);
+  $name = filter_var($me['displayName'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  $personMarkup = "<a rel='me' href='$url'>$name</a><div><img src='$img?sz=82'></div>";
+
   $optParams = array('maxResults' => 100);
   $activities = $plus->activities->listActivities('me', 'public', $optParams);
-
-  print 'Your Activities: <pre>' . print_r($activities, true) . '</pre>';
+  $activityMarkup = '';
+  foreach($activities['items'] as $activity) {
+    // These fields are currently filtered through the PHP sanitize filters.
+    // See http://www.php.net/manual/en/filter.filters.sanitize.php
+    $url = filter_var($activity['url'], FILTER_VALIDATE_URL);
+    $title = filter_var($activity['title'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+    $content = filter_var($activity['object']['content'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+    $activityMarkup .= "<div class='activity'><a href='$url'>$title</a><div>$content</div></div>";
+  }
 
   // The access token may have been updated lazily.
-  $_SESSION['token'] = $client->getAccessToken();
+  $_SESSION['access_token'] = $client->getAccessToken();
 } else {
   $authUrl = $client->createAuthUrl();
-  print "<a class='login' href='$authUrl'>Connect Me!</a>";
 }
+?>
+<!doctype html>
+<html>
+<head><link rel='stylesheet' href='style.css' /></head>
+<body>
+<header><h1>Google+ Sample App</h1></header>
+<div class="box">
+
+<?php if(isset($personMarkup)): ?>
+<div class="me"><?php print $personMarkup ?></div>
+<?php endif ?>
+
+<?php if(isset($activityMarkup)): ?>
+<div class="activities">Your Activities: <?php print $activityMarkup ?></div>
+<?php endif ?>
+
+<?php
+  if(isset($authUrl)) {
+    print "<a class='login' href='$authUrl'>Connect Me!</a>";
+  } else {
+   print "<a class='logout' href='?logout'>Logout</a>";
+  }
+?>
+</div>
+</body>
+</html>
