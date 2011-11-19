3b0ab170f948c71d58213eb09f179932e686d4fa
Joseph Harry <findarato@gmail.com>
2011-11-19 04:17:11 +0000

    Fixed the permission system to allow for easier reading as well as more program friendly permissions

--- sql/Permissions.sql
+++ sql/Permissions.sql
@@ -18,6 +18,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 CREATE TABLE IF NOT EXISTS `permissions` (
   `id` int(11) NOT NULL auto_increment,
+  `display` text NOT NULL,
   `permission` text NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `id` (`id`)
@@ -27,9 +28,9 @@ CREATE TABLE IF NOT EXISTS `permissions` (
 -- Dumping data for table `permissions`
 --
 
-INSERT INTO `permissions` (`id`, `permission`) VALUES
-(1, 'Administrator'),
-(2, 'Staff'),
-(3, 'Basic User'),
-(4, 'Department Head'),
-(5, 'No Access');
+INSERT INTO `permissions` (`id`, `display`, `permission`) VALUES
+(1, 'Administrator', 'ADMIN'),
+(2, 'Staff', 'STAFF'),
+(3, 'Basic User', 'USER'),
+(4, 'Department Head', 'MANAGER'),
+(5, 'No Access', 'NO_ACCESS');
