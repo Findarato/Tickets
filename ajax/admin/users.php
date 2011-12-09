34284c101fd4f750ca07fd9205db2ad31109afae
Joseph Harry <findarato@gmail.com>
2011-12-09 21:21:41 +0000

    Getting user information is now in for the back end.

--- ajax/admin/users.php
+++ ajax/admin/users.php
@@ -4,8 +4,24 @@
  * Page to manage user accounts, edit, view all things that have to do with editing accounts
  * 
  */
-include "../small_header.php";
+include "../../small_header.php";
 include "adminHeader.php";
 
 
+function getUser($id="all"){
+	$db = db::getInstance();
+	$json = array();
+	if($id=="all"){
+		$json["users"] = $db->Query("SELECT * FROM users",false,"assoc_array"); 
+		$json["permissions"] = $db->Query("SELECT up.user_id,up.permission_id,p.display FROM user_permissions AS up JOIN permissions AS p ON (up.permission_id=p.id)",false,"assoc_array");
+		
+	}else{
+		$json["users"] = $db->Query("SELECT * FROM users WHERE id=".$id,false,"assoc_array");
+		$json["permissions"] = $db->Query("SELECT up.user_id,up.permission_id,p.display FROM user_permissions AS up JOIN permissions AS p ON (up.permission_id=p.id) WHERE up.user_id=".$id,false,"assoc_array");
+	}
+	
+	return $json;
+}
 
+
+echo json_encode(getUser());
