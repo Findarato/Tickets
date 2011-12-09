d461690671671b170e43b425dd8f85e41f31855a
Joseph Harry <findarato@gmail.com>
2011-12-09 20:03:42 +0000

    Adding in the basic setup of the admin request pages, They all will require the adminHeader.php file which will check for admin rights before allowing any of the code to execute.  It also checks for no access, which trumps EVERYTHING

---
+++ ajax/admin/adminHeader.php
@@ -0,0 +1,19 @@
+<?Php
+/**
+ * Started December 9, 2011
+ * Just a small header so I do not have to type the admin permissions a lot of times
+ * 
+ */
+if(!in_array("ADMIN",$usr->getPermissions())){
+	if(in_array("NO_ACCESS", $usr->getPermissions())){
+		$response["error"] = "Permission Denied";
+		$response["message"] = "You do not have permission to view this file";
+		return json_encode($response);
+		die("no access");
+	}
+}else{
+	$response["error"] = "Permission Denied";
+	$response["message"] = "You do not have permission to view this file";
+	return json_encode($response);
+	die("no Access"); 
+}
---
+++ ajax/admin/users.php
@@ -0,0 +1,11 @@
+<?Php
+/**
+ * Started December 9, 2011
+ * Page to manage user accounts, edit, view all things that have to do with editing accounts
+ * 
+ */
+include "../small_header.php";
+include "adminHeader.php";
+
+
+
