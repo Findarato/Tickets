391535deda51d53a93dd0ea83de6badc72e26423
Joseph Harry <findarato@gmail.com>
2011-12-12 19:27:40 +0000

    Adding new permissions works for the display.

--- ajax/admin/users.php
+++ ajax/admin/users.php
@@ -11,6 +11,13 @@ include "adminHeader.php";
 function getUser($id="all"){
 	$db = db::getInstance();
 	$json = array();
+	$json["allPermissions"] = $db->Query("SELECT p.id,p.display FROM permissions AS p ORDER BY p.display",false,"assoc_array");
+	$holderArray = array();
+	foreach ($json["allPermissions"] as $k=>$data){
+		$holderArray[$data["id"]] = $data["display"];
+	}
+	$json["allPermissions"] = $holderArray;
+	unset($holderArray);
 	if($id=="all"){
 		$json["users"] = $db->Query("SELECT *, MD5(email_address) AS md5Email FROM users",false,"assoc_array"); 
 		$json["permissions"] = $db->Query("SELECT up.user_id,up.permission_id,p.display FROM user_permissions AS up JOIN permissions AS p ON (up.permission_id=p.id)",false,"assoc_array");
@@ -23,5 +30,6 @@ function getUser($id="all"){
 	return $json;
 }
 
+	
 
 echo json_encode(getUser());
--- js/modules/admin.js
+++ js/modules/admin.js
@@ -23,6 +23,7 @@ admin = {
 			Params.Content.html($("#generic").html());
 			Tlb = Params.Content.find("#ticketListbody");
 			$.getJSON("ajax/admin/users.php",{"allUsers":1},function(json){
+				Params.allPermissions = json.allPermissions;
 				var display = $("<div/>");
 				$.each(json.users,function(i,data){
 					var userDisplay = newUserTpl.clone();
@@ -35,23 +36,49 @@ admin = {
 					userDisplay.find("#userPic").attr("id","userPic-"+data.id).css("background-image","url(http://www.gravatar.com/avatar/"+data.md5Email+"?s=32&d=identicon&r=g)");
 					userDisplay.find("#body").attr("id","body-"+data.id).html(
 						data.email_address+"<br/>"+data.joined
-						
 					);
+					addPerm = newPermissionTpl.clone();
+					addPerm.attr("id",data.id+"addPerm").html(
+						$("<button/>",{"html":"Add Permission","id":"permAdd"+data.id,"class":"selectButton","css":{"padding":"0px","width":"105px","position":"relative"},value:0,"data-select-items":JSON.stringify(json.allPermissions)})
+					);
+					createSelect(addPerm.find("#permAdd"+data.id),function(value){
+						//alert(Params.allPermissions[value]);
+						var Tlb = Params.Content.find("#ticketListbody");
+						var paramName = Params.allPermissions[value];
+						if($("#permInfo_"+data.id+"_"+value).html() == null){
+							perm = newPermissionTpl.clone();
+							perm.attr("id","permInfo_"+data.id+"_"+value).html(paramName);
+							Tlb.find("#userDisplay"+data.id).append(perm);
+						}
+						
+						//alert(value)
+						//alert(data.id)
+					});
+					userDisplay.find("#userDisplay"+data.id).append(addPerm);
+				
+										
 					/*
-					
 					userDisplay.find("#tickCreatedBy").attr("id","user-"+value.id).html("By: "+value.firstname2+ " " + value.lastname2 );
 					userDisplay.find("#tickCreatedOn").attr("id","tickCreatedOn-"+value.id).html("On: "+value.created_on);
 					userDisplay.find("#tickCategory").attr("id","tickCategory-"+value.id).html(value.category);
 					userDisplay.find("#tickLocation").attr("id","tickLocation-"+value.id).html(value.locationName);
-					
 					*/
 					display.append(userDisplay);
 				}); // End of creating the user boxes
+				
 				$.each(json.permissions,function(i,data){
 					perm = newPermissionTpl.clone();
-					perm.attr("id","permInfo."+data.user_id+"."+data.display).html(data.display);
+					perm.attr("id","permInfo."+data.user_id+"."+data.display.replace(/ /g,"_")).html(data.display);
 					display.find("#userDisplay"+data.user_id).append(perm);
 				});// end of displaying the permissions
+				
+				
+
+				/*
+				display.find("#userDisplay"+data.user_id).append(addPerm);
+				*/
+				
+				
 				Tlb.append(display);		
 			});	
 	},"test":"cool"
